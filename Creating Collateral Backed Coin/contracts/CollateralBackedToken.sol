0017e5d5f479a3d6780e88484fc044775cd2a8e5
lisun85 <70138362+lisun85@users.noreply.github.com>
2021-04-01 16:31:08 +0800

    Update CollateralBackedToken.sol

---
+++ Creating Collateral Backed Coin/contracts/CollateralBackedToken.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC20/iERC20.sol;
+
+contract CollateralBackedToken is ERC20 {
+    IERC20 public collateral;
+    uint public price = 1;
+
+    constructor (address _collateral) ERC20('Collateral Backed Token', "CBT) {
+        collateral = IERC20(_collateral);
+    }
+
+    function deposit(uint collateralAmount) external {
+        collateral.transferFrom(msg.sender, address(this), collateralAmount);
+        _mint(msg.sender, collateralAmount * price)
+    }
+
+    function withdraw(uint tokenAMount) external {
+        require(balanceOf(msg.sender) >= tokenAMount, "balance)
+    }
+}
